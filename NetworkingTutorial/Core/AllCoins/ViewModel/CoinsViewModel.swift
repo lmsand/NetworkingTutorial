903414d700474ffb391018890af414476b02f587
lmsand <lmsand99@icloud.com>
2023-12-26 07:04:07 -0600

    Parsing JSON

--- NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -13,18 +13,31 @@ class CoinsViewModel: ObservableObject {
     @Published var price = ""
     
     init() {
-        fetchPrice()
+        fetchPrice(coin: "litecoin")
     }
     
-    func fetchPrice() {
-        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
+    func fetchPrice(coin: String) {
+        let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return } // url object
         
-        print("Fetching price")
+        print("Fetching price") // 1
         URLSession.shared.dataTask(with: url) { data, response, error in
-            print("Did receive \(data)")
+            print("Did receive \(data)") // 3
+            guard let data = data else { return }
+            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+            guard let value = jsonObject[coin] as? [String: Double] else {
+                print("Failed to parse value")
+                return
+            }
+            guard let price = value["usd"] else { return } // use guard statments to make optional vals non optional
+            
+            DispatchQueue.main.async {
+                self.coin = coin.capitalized
+                self.price = "$\(price)"
+            }
+            
         }.resume()
         
-        print("Did reach end of function")
+        print("Did reach end of function") // 2
     }
 }
