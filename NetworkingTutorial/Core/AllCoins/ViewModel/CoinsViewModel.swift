18ac175cc8720fdb2a2520910becdea5ab14754b
lmsand <lmsand99@icloud.com>
2023-12-26 15:20:05 -0600

    Error Handling

--- NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
+++ NetworkingTutorial/Core/AllCoins/ViewModel/CoinsViewModel.swift
@@ -11,6 +11,7 @@ class CoinsViewModel: ObservableObject {
     
     @Published var coin = ""
     @Published var price = ""
+    @Published var errorMessage: String?
     
     init() {
         fetchPrice(coin: "litecoin")
@@ -20,24 +21,39 @@ class CoinsViewModel: ObservableObject {
         let urlString = "https://api.coingecko.com/api/v3/simple/price?ids=\(coin)&vs_currencies=usd"
         guard let url = URL(string: urlString) else { return } // url object
         
-        print("Fetching price") // 1
         URLSession.shared.dataTask(with: url) { data, response, error in
-            print("Did receive \(data)") // 3
-            guard let data = data else { return }
-            guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
-            guard let value = jsonObject[coin] as? [String: Double] else {
-                print("Failed to parse value")
-                return
-            }
-            guard let price = value["usd"] else { return } // use guard statments to make optional vals non optional
-            
             DispatchQueue.main.async {
+                if let error = error {
+                    print("DEBUG: Failed with error \(error.localizedDescription)")
+                    self.errorMessage = error.localizedDescription
+                    return
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    self.errorMessage = "Bad HTTP Response"
+                    return
+                }
+                
+                guard httpResponse.statusCode == 200 else {
+                    self.errorMessage = "Failed to fetch with status code \(httpResponse.statusCode)"
+                    return
+                }
+                
+                print("DEBUG: Response code is \(httpResponse.statusCode)")
+                
+                guard let data = data else { return }
+                guard let jsonObject = try? JSONSerialization.jsonObject(with: data) as? [String: Any] else { return }
+                guard let value = jsonObject[coin] as? [String: Double] else {
+                    print("Failed to parse value")
+                    return
+                }
+                guard let price = value["usd"] else { return } // use guard statments to make optional vals non optional
+                
                 self.coin = coin.capitalized
                 self.price = "$\(price)"
             }
-            
+
         }.resume()
         
-        print("Did reach end of function") // 2
     }
 }
