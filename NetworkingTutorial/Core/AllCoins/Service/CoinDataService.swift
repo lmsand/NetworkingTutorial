d6c332383aca9cc931c34f3d9cec84811f513744
lmsand <lmsand99@icloud.com>
2023-12-26 19:27:50 -0600

    Custom Errors and Retain Cycles

--- NetworkingTutorial/Core/AllCoins/Service/CoinDataService.swift
+++ NetworkingTutorial/Core/AllCoins/Service/CoinDataService.swift
@@ -12,23 +12,44 @@ class CoinDataService { // background thread, no need for DispatchQueue.main { c
     private let urlString = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=20&page=1&sparkline=false&price_change_percentage=24h&locale=en"
     
     
-    func fetchCoinsWithResult(completion: @escaping(Result<[Coin], Error>) -> Void) {
+    func fetchCoinsWithResult(completion: @escaping(Result<[Coin], CoinAPIError>) -> Void) {
         guard let url = URL(string: urlString) else { return }
         
         URLSession.shared.dataTask(with: url) { data, response, error in
             if let error = error {
-                completion(.failure(error))
+                completion(.failure(.unknownError(error: error)))
                 return
             }
             
-            guard let data = data else { return }
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.requestFailed(description: "Request Failed")))
+                return
+            }
             
-            guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else { return } // decoding as a specified object, data comes back as array
+            guard httpResponse.statusCode == 200 else {
+                completion(.failure(.invalidStatusCode(statusCode: httpResponse.statusCode)))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.invalidData))
+                return
+            }
+            
+            do {
+                let coins = try? JSONDecoder().decode([Coin].self, from: data)
+                completion(.success(coins!))
+            } catch {
+                print("DEBUG: failed to decode with error \(error)") // get access to error in a catch automatically
+                completion(.failure(.jsonParsingFailure))
+            }
+            
+            //guard let coins = try? JSONDecoder().decode([Coin].self, from: data) else { return } // decoding as a specified object, data comes back as array
             
 //            for coin in coins {
 //                print("DEBUG: coin id \(coin.id)")
 //            }
-            completion(.success(coins))
+            
             
         }.resume()
     }
